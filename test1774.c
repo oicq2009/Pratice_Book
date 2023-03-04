78fb9e1fec4d83fa4b9f18c62e9f4bec3703ea10
foxny <70536706+oicq2009@users.noreply.github.com>
2023-03-04 19:31:10 +0900

    Create test1774.c

---
+++ test1774.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main (
+    void
+    ) 
+{
+    pid_t id = fork();
+    if (id == 0) {
+        int cnt = 5;   // 循环5次
+        // child
+        while (1) {
+            // 五秒之内运行状态
+            printf("我是子进程，我正在运行... Pid: %d\n", getpid());
+            sleep(1);
+
+            // 五秒之后子进程终止
+            cnt--;
+            if (cnt == 0) {
+                break; 
+            }
+        }
+
+        exit(13);   // 方便辨识，推出码我们设置为13，这是我们的预期结果
+    }
+    else {
+        printf("我是父进程: pid: %d，我将耐心地等待子进程！\n", getpid());
+        
+        int status = 0;   // 接收status
+        // ***** wait_pid 的使用
+        pid_t ret = waitpid(id, &status, 0);
+        if (ret > 0) {   // 等待成功
+            printf("等待成功，ret: %d, 我所等待的子进程退出码: %d\n", ret, (status>>8)&0xFF);
+        }
+
+    }
+}
+
+
+
+
+
+
+
