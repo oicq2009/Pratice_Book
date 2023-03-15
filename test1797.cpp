f067319742cd21ff573bb003b6d92a2259b1576a
foxny <70536706+oicq2009@users.noreply.github.com>
2023-03-15 22:36:28 +0900

    Create test1797.cpp

---
+++ test1797.cpp
@@ -0,0 +1,20 @@
+#include  <stdio.h>
+#include  <sys/types.h>
+#include  <sys/stat.h>
+#include  <fcntl.h>
+
+int main()
+{
+    int fd = open("log.txt", O_APPEND | O_CREAT, 0666);
+    if (fd < 0) {
+        perror("open");
+        return 1;
+    }
+
+    printf("fd: %d\n", fd);
+
+    return 0;
+}
+
+
+
